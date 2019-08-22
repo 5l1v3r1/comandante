49aa67243f2d0bf2a32dffbad4d0a854dc84893b
daniel.golabek@gmail.com <daniel.golabek@gmail.com>
2019-08-22 17:26:06 +0100

    Entity editor

--- src/Comandante.TestsWeb/Areas/Identity/Data/Debug4MvcNetCoreTestsWebContext.cs
+++ src/Comandante.TestsWeb/Areas/Identity/Data/Debug4MvcNetCoreTestsWebContext.cs
@@ -22,5 +22,35 @@ namespace Comandante.TestsWeb.Models
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Advert> Adverts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public int Rating { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+
+    public class Advert
+    {
+        public int Id { get; set; }
+        public Blog Blog { get; set; }
+        public int? NumberOfDisplays { get; set; }
+        public bool? IsActive { get; set; }
     }
 }
